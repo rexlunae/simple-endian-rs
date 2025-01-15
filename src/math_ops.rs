4a37e961a71bad0996014e7039ecb4c21111d6b1
Burkhard Mittelbach <wasabi37a@googlemail.com>
2025-01-15 20:06:30 +0100

    Impl math ops for all T

--- src/math_ops.rs
+++ src/math_ops.rs
@@ -7,8 +7,11 @@ use super::*;
 
 #[allow(unused_macros)]
 macro_rules! add_math_ops {
-    ($wrap_ty:ty) => {
-        impl Add for $wrap_ty {
+    ($wrap_ty:ident) => {
+        impl<T> Add for $wrap_ty<T>
+        where
+            T: Add<Output = T> + SpecificEndian<T>,
+        {
             type Output = Self;
 
             fn add(self, other: Self) -> Self {
@@ -16,13 +19,19 @@ macro_rules! add_math_ops {
             }
         }
 
-        impl AddAssign for $wrap_ty {
+        impl<T> AddAssign for $wrap_ty<T>
+        where
+            T: Add<Output = T> + SpecificEndian<T>,
+        {
             fn add_assign(&mut self, other: Self) {
                 *self = *self + other;
             }
         }
 
-        impl Mul for $wrap_ty {
+        impl<T> Mul for $wrap_ty<T>
+        where
+            T: Mul<Output = T> + SpecificEndian<T>,
+        {
             type Output = Self;
 
             fn mul(self, other: Self) -> Self {
@@ -30,13 +39,19 @@ macro_rules! add_math_ops {
             }
         }
 
-        impl MulAssign for $wrap_ty {
+        impl<T> MulAssign for $wrap_ty<T>
+        where
+            T: Mul<Output = T> + SpecificEndian<T>,
+        {
             fn mul_assign(&mut self, other: Self) {
                 *self = *self * other;
             }
         }
 
-        impl Div for $wrap_ty {
+        impl<T> Div for $wrap_ty<T>
+        where
+            T: Div<Output = T> + SpecificEndian<T>,
+        {
             type Output = Self;
 
             fn div(self, other: Self) -> Self {
@@ -44,20 +59,30 @@ macro_rules! add_math_ops {
             }
         }
 
-        impl DivAssign for $wrap_ty {
+        impl<T> DivAssign for $wrap_ty<T>
+        where
+            T: Div<Output = T> + SpecificEndian<T>,
+        {
             fn div_assign(&mut self, other: Self) {
                 *self = *self / other;
             }
         }
 
-        impl Sub for $wrap_ty {
+        impl<T> Sub for $wrap_ty<T>
+        where
+            T: Sub<Output = T> + SpecificEndian<T>,
+        {
             type Output = Self;
 
             fn sub(self, other: Self) -> Self {
                 Self::from(self.to_native() - other.to_native())
             }
         }
-        impl SubAssign for $wrap_ty {
+
+        impl<T> SubAssign for $wrap_ty<T>
+        where
+            T: Sub<Output = T> + SpecificEndian<T>,
+        {
             fn sub_assign(&mut self, other: Self) {
                 *self = *self - other;
             }
@@ -65,71 +90,8 @@ macro_rules! add_math_ops {
     };
 }
 
-#[cfg(feature = "big_endian")]
-mod be {
-    use super::*;
-    #[cfg(feature = "byte_impls")]
-    mod bytes {
-        use super::*;
-        add_math_ops!(BigEndian<u8>);
-        add_math_ops!(BigEndian<i8>);
-    }
-
-    #[cfg(feature = "integer_impls")]
-    mod integers {
-        use super::*;
-        add_math_ops!(BigEndian<u16>);
-        add_math_ops!(BigEndian<i16>);
-        add_math_ops!(BigEndian<u32>);
-        add_math_ops!(BigEndian<i32>);
-        add_math_ops!(BigEndian<u64>);
-        add_math_ops!(BigEndian<i64>);
-        add_math_ops!(BigEndian<u128>);
-        add_math_ops!(BigEndian<i128>);
-        add_math_ops!(BigEndian<usize>);
-        add_math_ops!(BigEndian<isize>);
-    }
-
-    #[cfg(feature = "float_impls")]
-    mod floats {
-        use super::*;
-        add_math_ops!(BigEndian<f32>);
-        add_math_ops!(BigEndian<f64>);
-    }
-}
-
-#[cfg(feature = "little_endian")]
-mod le {
-    use super::*;
-    #[cfg(feature = "byte_impls")]
-    mod bytes {
-        use super::*;
-        add_math_ops!(LittleEndian<u8>);
-        add_math_ops!(LittleEndian<i8>);
-    }
-
-    #[cfg(feature = "integer_impls")]
-    mod integers {
-        use super::*;
-        add_math_ops!(LittleEndian<u16>);
-        add_math_ops!(LittleEndian<i16>);
-        add_math_ops!(LittleEndian<u32>);
-        add_math_ops!(LittleEndian<i32>);
-        add_math_ops!(LittleEndian<u64>);
-        add_math_ops!(LittleEndian<i64>);
-        add_math_ops!(LittleEndian<u128>);
-        add_math_ops!(LittleEndian<i128>);
-        add_math_ops!(LittleEndian<usize>);
-        add_math_ops!(LittleEndian<isize>);
-    }
-
-    #[cfg(feature = "float_impls")]
-    mod floats {
-        use super::*;
-        add_math_ops!(LittleEndian<f32>);
-        add_math_ops!(LittleEndian<f64>);
-    }
-}
+add_math_ops!(LittleEndian);
+add_math_ops!(BigEndian);
 
 #[cfg(test)]
 mod tests {
