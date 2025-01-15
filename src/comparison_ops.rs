ee9501522af567df493b7cf0383a109e1f59eaf3
Burkhard Mittelbach <wasabi37a@googlemail.com>
2025-01-15 15:03:42 +0100

    Impl comparision_ops for all T

--- src/comparison_ops.rs
+++ src/comparison_ops.rs
@@ -5,99 +5,39 @@ use core::cmp::Ordering;
 #[allow(unused_imports)]
 use super::*;
 
-/// For types that have PartialEq.
-#[allow(unused_macros)]
-macro_rules! add_equality_ops {
-    ($wrap_ty:ty) => {
-        impl PartialOrd for $wrap_ty {
-            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-                self.to_native().partial_cmp(&other.to_native())
-            }
-        }
-    };
-}
-
-// The floats can only have PartialOrd, not Ord, because they only have PartialEq and not Eq.
-#[cfg(feature = "float_impls")]
-mod float_comps {
-    use super::*;
-    #[cfg(feature = "big_endian")]
-    mod be {
-        use super::*;
-        add_equality_ops!(BigEndian<f32>);
-        add_equality_ops!(BigEndian<f64>);
-    }
-    #[cfg(feature = "little_endian")]
-    mod le {
-        use super::*;
-        add_equality_ops!(LittleEndian<f32>);
-        add_equality_ops!(LittleEndian<f64>);
+impl<T> PartialOrd for BigEndian<T>
+where
+    T: PartialOrd + SpecificEndian<T>,
+{
+    fn partial_cmp(&self, other: &BigEndian<T>) -> Option<Ordering> {
+        self.to_native().partial_cmp(&other.to_native())
     }
 }
 
-/// For types that implement Eq.
-#[allow(unused_macros)]
-macro_rules! add_full_equality_ops {
-    ($wrap_ty:ty) => {
-        impl Ord for $wrap_ty {
-            fn cmp(&self, other: &Self) -> Ordering {
-                self.to_native().cmp(&other.to_native())
-            }
-        }
-        add_equality_ops!($wrap_ty);
-    };
-}
-
-// We have to separate Ord because f32/64 don't have Eq trait.
-#[cfg(feature = "integer_impls")]
-mod integer_comps {
-    use super::*;
-    #[cfg(feature = "big_endian")]
-    mod be {
-        use super::*;
-        add_full_equality_ops!(BigEndian<u16>);
-        add_full_equality_ops!(BigEndian<i16>);
-        add_full_equality_ops!(BigEndian<u32>);
-        add_full_equality_ops!(BigEndian<i32>);
-        add_full_equality_ops!(BigEndian<u64>);
-        add_full_equality_ops!(BigEndian<i64>);
-        add_full_equality_ops!(BigEndian<u128>);
-        add_full_equality_ops!(BigEndian<i128>);
-        add_full_equality_ops!(BigEndian<usize>);
-        add_full_equality_ops!(BigEndian<isize>);
-    }
-    #[cfg(feature = "little_endian")]
-    mod le {
-        use super::*;
-        add_full_equality_ops!(LittleEndian<u16>);
-        add_full_equality_ops!(LittleEndian<i16>);
-        add_full_equality_ops!(LittleEndian<u32>);
-        add_full_equality_ops!(LittleEndian<i32>);
-        add_full_equality_ops!(LittleEndian<u64>);
-        add_full_equality_ops!(LittleEndian<i64>);
-        add_full_equality_ops!(LittleEndian<u128>);
-        add_full_equality_ops!(LittleEndian<i128>);
-        add_full_equality_ops!(LittleEndian<usize>);
-        add_full_equality_ops!(LittleEndian<isize>);
+impl<T> PartialOrd for LittleEndian<T>
+where
+    T: PartialOrd + SpecificEndian<T>,
+{
+    fn partial_cmp(&self, other: &LittleEndian<T>) -> Option<Ordering> {
+        self.to_native().partial_cmp(&other.to_native())
     }
 }
 
-#[cfg(feature = "byte_impls")]
-mod byte_comps {
-    use super::*;
-    #[cfg(feature = "big_endian")]
-    mod be {
-        use super::*;
-        add_full_equality_ops!(BigEndian<bool>);
-        add_full_equality_ops!(BigEndian<u8>);
-        add_full_equality_ops!(BigEndian<i8>);
+impl<T> Ord for BigEndian<T>
+where
+    T: Ord + SpecificEndian<T>,
+{
+    fn cmp(&self, other: &BigEndian<T>) -> Ordering {
+        self.to_native().cmp(&other.to_native())
     }
-    #[cfg(feature = "little_endian")]
-    mod le {
-        use super::*;
-        add_full_equality_ops!(LittleEndian<bool>);
-        add_full_equality_ops!(LittleEndian<u8>);
-        add_full_equality_ops!(LittleEndian<i8>);
+}
+
+impl<T> Ord for LittleEndian<T>
+where
+    T: Ord + SpecificEndian<T>,
+{
+    fn cmp(&self, other: &LittleEndian<T>) -> Ordering {
+        self.to_native().cmp(&other.to_native())
     }
 }
 
